b2083223baef39b78249ffc52303ab031a799669
Roberto Morell <rmo_lepe_96@hotmail.com>
2025-01-26 21:52:12 +0100

    App flask

--- src/app_modelo.py
+++ src/app_modelo.py
@@ -15,16 +15,14 @@ class_dict = {
 }
 
 @app.route('/', methods=['GET', 'POST'])
-
 def index():
+    pred_comment = None  # Inicializar predicción
     if request.method == 'POST':
-
         text = request.form['text']
-        prediction = model.predict(text)
-        pred_comment = class_dict (prediction[0])
-
-    else:
-        pred = None
+        if text:  # Verificar si se ingresó texto
+            # Asegurarse de que el texto se pase como lista al modelo
+            prediction = model.predict([text])
+            pred_comment = class_dict[prediction[0]]  # Obtener el resultado correcto
 
     return render_template('index.html', prediction=pred_comment)
 
